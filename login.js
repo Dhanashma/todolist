9ad8e85567db45735f343e37f70f674b0e5df489
dhanashma <dhanashma@gmail.com>
2021-11-28 06:23:39 +0530

    second commit

--- login.js
+++ login.js
@@ -1,26 +1,19 @@
 function validate(callback)
 	{
-        console.log("Hi");
-
+       
         var username= document.getElementById("u_name").value;
-        var pwd= document.getElementById("pwd").value;  
-        
-            
+        var pwd= document.getElementById("pwd").value;    
     
             if(username.trim()==""||pwd.trim()==""){
-                alert("Fields cannot be empty");
-                
+                alert("Fields cannot be empty");         
             }
            
-            else if(username == "admin" && pwd == "12345"){
-            
-                loginpage();
-                
+            else if(username == "admin" && pwd == "12345"){   
+                callback();       
             }
+           
             else{
-                console.log("login failed");
-
-                alert("Invalid");
+                  alert("Invalid username or password");
             }
                 
     }
--- todo.js
+++ todo.js
@@ -1,42 +1,35 @@
-// // Create a "close" button and append it to each list item
-// var myNodelist = document.getElementsByTagName("LI");
-// var i;
-// for (i = 0; i < myNodelist.length; i++) {
-//   var span = document.createElement("SPAN");
-//   var txt = document.createTextNode("\u00D7");
-//   span.className = "close";
-//   span.appendChild(txt);
-//   myNodelist[i].appendChild(span);
-// }
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
 
-// Click on a close button to hide the current list item
+
 function myClick(){
-  console.log("buttoncicked")
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function(){
       if(this.readyState == 4 && this.status == 200){
           var response = JSON.parse(this.responseText);
           var output = "";
           for(var i=0; i<response.length;i++){
-              output +=  "<li id='list' class>"+response[i].title+"<span class='close'>×</span></li>";
-          }
-          
+              output += "<li id='li' class><input class='check' type='checkbox'><label>"+response[i].title+
+  "</label><span class='close'>×</span></li>"
+            }
+          // Click on a close button to hide the current list item
           document.getElementById("container").innerHTML ="<ul id='myUL'>"+output+"</ul>";
           var close = document.getElementsByClassName("close");
           var i;
           for (i = 0; i < close.length; i++) {
             close[i].onclick = function() {
-              console.log("called");
               var div = this.parentElement;
               div.style.display = "none";
             }
           }
-          var list = document.querySelector('ul');
-          list.addEventListener('click', function(ev) {
-          if (ev.target.tagName === 'LI') {
-          ev.target.classList.toggle('checked');
-          }
-          }, false);
         }      
   }
   xhttp.open("GET",'https://jsonplaceholder.typicode.com/todos',true);
@@ -44,39 +37,24 @@ function myClick(){
   
 }
 
-$( document ).ready(function() {
-  console.log( "document loaded" );
-});
-
-
-// Add a "checked" symbol when clicking on a list item
-$( document ).ready(function() {
-  console.log( "document loaded" );
-  
-});
-
-
-// document.getElementById("list").addEventListener("click", function() {
-//   alert("Hello World!");
-// });
-// function ev() {
-//   if (ev.target.tagName === 'LI') {
-//     ev.target.classList.toggle('checked');
-//   }
-// }, false);
-
-
-
 // Create a new list item when clicking on the "Add" button
 function newElement() {
   var li = document.createElement("li");
+  li.setAttribute("id","li");
+  li.setAttribute("class","");
+  var checkbox = document.createElement("input");
+  var label = document.createElement("label");
+  checkbox.setAttribute("type","checkbox");
+  checkbox.setAttribute("class","check");
   var inputValue = document.getElementById("myInput").value;
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
     alert("You must write something!");
   } else {
-    document.getElementById("myUL").appendChild(li);
+    document.getElementById("myUL").appendChild(li).appendChild(checkbox);
+    li.appendChild(label);
+    label.appendChild(t);
   }
   document.getElementById("myInput").value = "";
 
@@ -85,6 +63,7 @@ function newElement() {
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
+          var close = document.getElementsByClassName("close");
 
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function() {
@@ -92,5 +71,7 @@ function newElement() {
       div.style.display = "none";
     }
   }
+  
+
 }
 
